aea9815aac7c81508b7b65ff8c2da95846572e83
Andy Schmidt <andreas.schmidt@yasoon.com>
2026-03-06 16:13:45 +0100

    Update exports

--- express-zod-api/src/index.ts
+++ express-zod-api/src/index.ts
@@ -9,7 +9,7 @@ export {
 export { getMessageFromError } from "./common-helpers";
 export { ensureHttpError, getPublicErrorMessage } from "./result-helpers";
 export { BuiltinLogger } from "./builtin-logger";
-export { Middleware } from "./middleware";
+export { Middleware, ExpressMiddleware, AbstractMiddleware } from "./middleware";
 export {
   ResultHandler,
   defaultResultHandler,
